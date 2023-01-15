a3074e268495cab15acb6efad3c93985b6d32a32
ghubisz <gab.op.pl@op.pl>
2023-01-15 14:32:40 +0100

    commit - modele

--- Oddam-w-dobre-r-ce/mysite/polls/models.py
+++ Oddam-w-dobre-r-ce/mysite/polls/models.py
@@ -1,3 +1,37 @@
+import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import dateutil.utils
 
-# Create your models here.
+class Category(models.Model):
+    name= models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Institution(models.Model):
+    name=models.CharField(max_length=128)
+    description = models.TextField()
+    #type=
+    categories= models.ForeignKey(to=Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Donation(models.Model):
+    quantity= models.DecimalField(max_digits=6, decimal_places=2)
+    categories=models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    institution
+    address = models.TextField()
+    phone_number = models.DecimalField(max_digits=9)
+    city = models.TextField()
+    zip_code = models.DecimalField(max_digits=5)
+    pick_up_date = models.DateField()
+    pick_up_time
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
