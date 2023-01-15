ffbfa000045452db6d00617afbfe6e63330ce308
ghubisz <gab.op.pl@op.pl>
2023-01-15 17:17:13 +0100

    commit - widoki etc

--- Oddam-w-dobre-r-ce/mysite/polls/models.py
+++ Oddam-w-dobre-r-ce/mysite/polls/models.py
@@ -5,32 +5,42 @@ from django.utils import timezone
 import dateutil.utils
 
 class Category(models.Model):
-    name= models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
 
 class Institution(models.Model):
-    name=models.CharField(max_length=128)
+
+    class Type(models.IntegerChoices):
+
+        FUNDACJA = 1
+        ORGANIZACJA POZARZĄDOWA= 2
+        ZBIÓRKA LOKALNA = 3
+
+        type = models.IntegerField(Type.choices, default= FUNDACJA)
+
+    name = models.CharField(max_length=128)
     description = models.TextField()
-    #type=
-    categories= models.ForeignKey(to=Category, on_delete=models.CASCADE)
+
+    categories = models.ForeignKey(to=Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Donation(models.Model):
-    quantity= models.DecimalField(max_digits=6, decimal_places=2)
-    categories=models.ForeignKey(to=Category, on_delete=models.CASCADE)
-    institution
+    quantity = models.DecimalField(max_digits=6, decimal_places=2)
+    categories = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    institution = models.ForeignKey(to=Institution, on_delete=models.CASCADE)
     address = models.TextField()
+    address_home_nr = models.DecimalField(max_digits=6, decimal_places=3)
     phone_number = models.DecimalField(max_digits=9)
     city = models.TextField()
     zip_code = models.DecimalField(max_digits=5)
     pick_up_date = models.DateField()
-    pick_up_time
+    pick_up_time = models.IntegerField()
     pick_up_comment = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=NULL)
 
     def __str__(self):
         return self.name
