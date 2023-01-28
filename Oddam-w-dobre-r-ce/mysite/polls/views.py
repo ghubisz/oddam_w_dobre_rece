c7318718b33281bcd4bfb7fd72eb62cd4c859fd0
ghubisz <gab.op.pl@op.pl>
2023-01-28 12:42:43 +0100

    dynamiczne statystyki

--- Oddam-w-dobre-r-ce/mysite/polls/views.py
+++ Oddam-w-dobre-r-ce/mysite/polls/views.py
@@ -6,9 +6,29 @@ from .models import Institution
 from .models import Donation
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import get_user
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth import get_user_model
 import ipdb
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.core.paginator import Paginator
+from django.db.models import ExpressionWrapper, F, DateTimeField
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import View
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    UpdateView,
+    FormView,
+    TemplateView
+)
 
 
 def index(request):
@@ -31,10 +51,24 @@ def Landing_Page(request):
         institutions = len(set([donation.institution_id for donation in
                                 donations]))
 
+        found_paginator = Paginator(foundations, 5)
+        ngos = Institution.objects.filter(institution_type='NGOV')
+        ngos_paginator = Paginator(ngos, 5)
+        charity = Institution.objects.filter(institution_type='CHAR')
+        charity_paginator = Paginator(charity, 5)
+
+        page_number = request.GET.get('foundations')
+        foundations = found_paginator.get_page(page_number)
+        page_number = request.GET.get('page2')
+        ngos = ngos_paginator.get_page(page_number)
+        page_number = request.GET.get('page3')
+        charities = charity_paginator.get_page(page_number)
+
         context = {
             'quantity': quantity,
             'institutions': institutions,
             'foundations': foundations,
+            'ngos': ngos,
             'charitiy': charity
 #            'queryset': queryset
 #            'user_id': request.user.id
