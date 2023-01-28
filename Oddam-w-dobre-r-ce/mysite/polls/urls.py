3399601b1cfb768c24a537c9fd1c7e6977cbdda5
ghubisz <gab.op.pl@op.pl>
2023-01-28 23:02:08 +0100

    działająca wersja. 5 kroków nie działa poprawnie

--- Oddam-w-dobre-r-ce/mysite/polls/urls.py
+++ Oddam-w-dobre-r-ce/mysite/polls/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from django.urls import include
-from .views import *
 from . import views
 
 urlpatterns = [
 #    path('', views.entry_welcome, name='welcome'),
 #    path('products/', views.entry_welcome, name='products'),
-    path('', views.Landing_Page,name='Home'),
+    path('', views.Landing_Page.as_view(),name='Home'),
 #    path('admin/', admin.site.urls),
-    path('landing_page/', views.Landing_Page, name='landing_page'),
+    path('landing_page/', views.Landing_Page.as_view(), name='landing_page'),
     path('form/', views.AddDonation, name='form'),
-    path('login/', views.Login, name='login'),
-    path('register/', views.Register, name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('register/', views.Register.as_view(), name='register'),
     path('index/', views.index, name='index')
 
 ]
