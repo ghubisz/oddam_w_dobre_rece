9ea6587e7d0278b8f55931f837627bf01a01a324
ghubisz <gab.op.pl@op.pl>
2023-01-29 18:08:56 +0100

    paginacja

--- Oddam-w-dobre-r-ce/mysite/polls/urls.py
+++ Oddam-w-dobre-r-ce/mysite/polls/urls.py
@@ -5,13 +5,14 @@ from . import views
 urlpatterns = [
 #    path('', views.entry_welcome, name='welcome'),
 #    path('products/', views.entry_welcome, name='products'),
-    path('', views.Landing_Page.as_view(),name='Home'),
+    path('', views.Index.as_view(), name='home'),
 #    path('admin/', admin.site.urls),
     path('landing_page/', views.Landing_Page.as_view(), name='landing_page'),
     path('form/', views.AddDonation, name='form'),
     path('login/', views.Login.as_view(), name='login'),
-    path('register/', views.Register.as_view(), name='register'),
-    path('index/', views.index, name='index')
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('index/', views.Index.as_view(), name='index'),
+    path('activate/<uid64>/<token>/', views.ActivateAccount.as_view(), name='activate')
 
 ]
 
